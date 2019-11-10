03005e546036a37b73ddc4143526c8eaa4e010b3
Thomas Frank <thomasfrank@Thomass-MacBook-2.local>
2019-11-10 15:51:33 +0100

    initial code

---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+
+function terminateUrlWithSlash(url) {
+  let p = url.split('?');
+  p[0] += p[0].slice(-1) === '/' ? '' : '/';
+  return p.join('?');
+}
+
+module.exports = (settings = {}) => {
+  let mem = [];
+  let endpoint = settings.endpoint || '/sse';
+  let script = settings.script || '/sse.js';
+  let scriptSource = fs.readFileSync('./sse.js', 'utf-8');
+  endpoint = terminateUrlWithSlash(endpoint);
+  script = terminateUrlWithSlash(script);
+  scriptSource = scriptSource.split('export default ')[1];
+
+  function enforceOneSessionPerBrowser(newReq, newRes) {
+    mem.forEach(({ req, res }) => {
+      if (!req.session) { return; }
+      if (
+        req.query.browserId === newReq.query.browserId &&
+        req.session.id !== newReq.session.id
+      ) {
+        res.end();
+      }
+    });
+  }
+
+  return {
+
+    SSE(req, res, next) {
+      let url = terminateUrlWithSlash(req.url);
+      if(url === script){
+        res.set({
+          'Content-Type': 'javascript/application',
+        });
+        res.send(scriptSource);
+        return;
+      }
+      if (url.indexOf(endpoint) !== 0) { next(); return; }
+      enforceOneSessionPerBrowser(req, res);
+      res.set({
+        Connection: 'keep-alive',
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache'
+      });
+      let item = { req, res };
+      mem.push(item);
+      req.on('close', () => {
+        let index = mem.indexOf(item);
+        index >= 0 && mem.splice(index, 1);
+      });
+    },
+
+    send(to, eventType, data, filter = (() => true)) {
+      if (typeof to !== 'function' && to !== 'all') {
+        throw (new Error('Send to "all" or a filter function filtering on req'));
+      }
+      if (typeof to !== 'function') {
+        to = () => true;
+      }
+      mem.filter(({ req }) => to(req)).forEach(item =>
+        item.res.write(
+          `event: ${eventType}\n` +
+          `data: ${JSON.stringify(data)}\n\n`
+        )
+      );
+    },
+
+    openConnections() { return mem.length; },
+
+    openSessions() {
+      let ids = {};
+      mem.forEach(x => x.req.session && (ids[x.req.session.id] = 1));
+      return Object.keys(ids).length;
+    }
+  }
+}
---
+++ sse.js
@@ -0,0 +1,56 @@
+export default class SSE {
+
+  constructor(endpoint) {
+    this.listeners = [];
+    this.restart(endpoint);
+  }
+
+  restart(endPoint) {
+    this.endPointCalc(endPoint);
+    if(this.longpolling){}
+    this.stream && this.stream.close();
+    this.stream = new EventSource(this.endPoint);
+    this.stream.addEventListener('error', () => {
+      this.addListeners();
+    });
+  }
+
+  listen(eventType, callback) {
+    this.listeners.push({ eventType, callback });
+    this.addListeners();
+    return this.listeners.length - 1;
+  }
+
+  unlisten(listenerIndex) {
+    this.listeners.splice(listenerIndex, 1);
+  }
+
+  addListeners() {
+    this.restart();
+    [...new Set(this.listeners.map(x => x.eventType))].forEach(x => {
+      this.stream.addEventListener(x, (event) => {
+        let data = event.data;
+        try { data = JSON.parse(data) } catch (e) { }
+        this.listeners
+          .filter(x => x.eventType === event.type)
+          .forEach(x => x.callback(data));
+      });
+    });
+  }
+
+  browserId() {
+    let s = window.localStorage;
+    if (!s) { return Math.random(); }
+    s.sseBrowserId = s.sseBrowserId || Math.random();
+    return s.sseBrowserId;
+  }
+
+  endPointCalc(endPoint){
+    this.endPoint = endPoint || this.endPoint || '/sse';
+    if (!this.endPoint.includes('browserId=')) {
+      this.endPoint += (this.endPoint.includes('?') ? '&' : '?')
+        + 'browserId=' + this.browserId();
+    }
+  }
+
+}
